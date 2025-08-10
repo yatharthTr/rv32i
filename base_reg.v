fcbcda3c0008893c3d3372019e0ceb580d07fa85
yatharthTr <tripathi.rishi2211@gmail.com>
2025-08-10 08:04:09 +0530

    Update base_reg.v

--- base_reg.v
+++ base_reg.v
@@ -1,10 +1,10 @@
 module base_reg(
-    input [5:0]rs_addr_1,
-    input [5:0]rs_addr_2,
+    input [4:0]rs_addr_1,
+    input [4:0]rs_addr_2,
     input clk,
-    input [5:0]rd_addr,
+    input [4:0]rd_addr,
     input [31:0]rd_val,
-    input i_ce,
+    // input i_ce,
     // input [31:0]rd_val,
     input i_wenable,
     input i_renable,
@@ -15,6 +15,13 @@ module base_reg(
 reg [31:0] reg_file[31:1];
 reg [4:0] o_rs_addr_1;
 reg [4:0] o_rs_addr_2;
+integer i;
+initial begin
+    for (i = 1; i < 32; i = i + 1) begin
+        reg_file[i] = i * 10;  
+    end
+end
+
 always @(posedge clk) begin
   if(rd_addr && i_wenable) begin
     reg_file[rd_addr]<=rd_val;
@@ -22,8 +29,8 @@ always @(posedge clk) begin
   if(i_renable) begin
     o_rs_addr_1<=rs_addr_1;
     o_rs_addr_2<=rs_addr_2;
-    end
+  end
 end
-assign rs_val1=o_rs_addr_1==0?0:reg_file[rs_addr_1];
-assign rs_val2=o_rs_addr_2==0?0:reg_file[rs_addr_2];
+assign rs_val1=o_rs_addr_1==0?0:reg_file[o_rs_addr_1];
+assign rs_val2=o_rs_addr_2==0?0:reg_file[o_rs_addr_2];
 endmodule
